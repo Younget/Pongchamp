01776c18fa77efbcae2d0522e22bdd51dafa8c41
Younget <48969941+Younget@users.noreply.github.com>
2019-11-07 08:29:45 -0800

    I added the menu screen to the actual project. It doesn't work yet until we fix compiler errors (Which are based around the ball destroy code). -Ethan Young 11/7/19

---
+++ Pong/Assets/Scripts/SceneChangePong.cs
@@ -0,0 +1,33 @@
+﻿/* By: Ethan Young
+ * Last Updated: 11/7/19
+ * Description: Changes the scene from menu to Pong 
+*/
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangePong : MonoBehaviour
+{
+    public string NextScene = "Pong";
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(NextScene);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
