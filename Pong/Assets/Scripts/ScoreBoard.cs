d2075d8c0eb7fb23b6d10ef898498b09ce0a401b
ComradeHappyPug <esther.strathy@digipen.edu>
2019-11-08 07:27:35 -0800

    uploading the working version

--- Pong/Assets/Scripts/ScoreBoard.cs
+++ Pong/Assets/Scripts/ScoreBoard.cs
@@ -8,7 +8,7 @@
 
 
 
- 
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,7 +16,7 @@ using UnityEngine.UI;
 
 public class ScoreBoard : MonoBehaviour
 {
-    public Text txt;
+    
     // Start is called before the first frame update
     void Start()
     {
