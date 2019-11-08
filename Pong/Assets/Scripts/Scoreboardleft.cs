9d4cd19120df418393702a6026d80276b8d727d9
ComradeHappyPug <esther.strathy@digipen.edu>
2019-11-08 07:30:51 -0800

    giving you the scene stuffs

---
+++ Pong/Assets/Scripts/Scoreboardleft.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class Scoreboardleft : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateScore();
+    }
+    public void UpdateScore()
+    {
+        Text myTextleft = GetComponent<Text>();
+        myTextleft.text = FindObjectOfType<balldestroy>().scoreleft.ToString();
+    }
+}
---
+++ Pong/Assets/Scripts/balldestroy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class balldestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float scoreleft = 0f;
+    public float scoreright = 0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (transform.position.x > 0)
+            scoreleft++;
+        if (transform.position.x < 0)
+            scoreright++; 
+        Destroy(gameObject);
+    }
+}
