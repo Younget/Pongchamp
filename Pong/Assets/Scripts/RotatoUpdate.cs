00cde94eb166d06ce0efd6d87564f3090be3a488
Younget <48969941+Younget@users.noreply.github.com>
2019-11-15 08:20:22 -0800

    fonttime dog and rotate too

---
+++ Pong/Assets/Scripts/RotatoUpdate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotato : MonoBehaviour
+{
+   balldestroy score;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = FindObjectOfType<balldestroy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (score.scoreleft >= 7 || score.scoreright >= 7)
+        {
+            transform.Rotate(0, 0, 20 * Time.deltaTime);
+        }
+    }
+}
